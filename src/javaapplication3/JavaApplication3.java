1d4183c5dc141f949544707e6bc30962f473a054
1 <1@172.20.10.3>
2016-11-15 09:08:44 +0200

    4456

---
+++ src/javaapplication3/JavaApplication3.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author 1
+ */
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Voloshko Anna Ruslanovna");
+        System.out.println("Voloshko" +" Anna" +" Ruslanovna");
+        System.out.println("11.04.1998");
+        System.out.println("Voloshko Anna Ruslanovna"); //ФИО
+        System.out.println("11.04.1998"); /*дата
+        рождения
+        */
+        // TODO code application logic here
+    }
+    
+}
